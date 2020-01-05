3220450a5b1c7ed2fe5e11340084cb73d0346c8f
opm-ok <ranbirmore@gmail.com>
2020-01-05 03:09:43 -0500

    Added insertion and shell sort

--- src/Main.java
+++ src/Main.java
@@ -6,20 +6,8 @@ public class Main {
 
         int[] intArray = {20, 35, -15, 7, 55, 1, -22 }; // indexes: 6
 
-        // Bubble Sort
-        System.out.println(Arrays.toString(bubbleSort(intArray)));
-
-        // Selection Sort
-        System.out.println(Arrays.toString(selectionSort(intArray)));
-
-        // Insertion Sort
-        System.out.println(Arrays.toString(insertionSort(intArray)));
-
-        // Insertion Sort
         System.out.println(Arrays.toString(shellSort(intArray)));
-
     }
-
 
     // Swap 2 integers in an array
     private static void swap(int[] arr, int i, int j){
@@ -31,26 +19,68 @@ public class Main {
         }
     }
 
-    // Bubble Sort
-    private static int[] bubbleSort(int[] intArray){
-        // unsortedPartitionIndex (last index of the unsorted array)
-        // i = 0, index used to traverse the array from left to right
+    // Shell Sort
+//    private static int[] shellSort(int[] intArray){
+//        // Variation of Insertion sort
+//        // In-place algorithm
+//        // O(n2) worst case, but it can perform much better than that (depends on the gap)
+//        // Unstable algorithm
+//
+//        for (int gap = intArray.length / 2; gap > 0; gap /= 2) {
+//
+//            for (int i = gap; i < intArray.length ; i++) {
+//                int checkElement = intArray[i];
+//
+//                int j = i;
+//
+//                while (j >= gap && intArray[j - gap] > checkElement){
+//                    intArray[j] = intArray[j - gap];
+//                    j -= gap;
+//                }
+//                intArray[j] = checkElement;
+//            }
+//        }
+//        return intArray;
+//    }
 
-        // In-place algorithm (do not need to create another array to implement the algorithm. Saves memory
-        // O(n2) time complexity - quadratic
-        // Algorithm degrades quickly
-        // Stable algorithm
+    // Shell Sort
+    private static int[] shellSort(int[] intArray){
 
-        for (int lastUnsortedIndex = intArray.length - 1; lastUnsortedIndex > 0; lastUnsortedIndex--){
+        for (int gap = intArray.length / 2; gap > 0; gap /= 2){
 
-            for (int i = 0; i < lastUnsortedIndex; i++){
-                if (intArray[i] > intArray[i + 1]){
-                    swap(intArray, i, i +1);
+            for (int i = gap; i < intArray.length; i++){
+
+                int selectedElement = intArray[i];
+                int j = i;
+
+                while (j >= gap && intArray[j - gap] > selectedElement){
+                    intArray[j] = intArray[j - gap];
+                    j -= gap;
                 }
+                intArray[j] = selectedElement;
             }
         }
-
         return intArray;
+    }
+
+    // Insertion Sort
+    private static int[] insertionSort(int[] intArray){
+
+        // In-place, stable algorithm, O(n2) quadratic time complexity
+        // if array was sorted in decreasing order, every element needs to be swapped
+
+        for (int firstUnsortedIndex = 1; firstUnsortedIndex < intArray.length; firstUnsortedIndex++) {
+
+            int j = firstUnsortedIndex;
+
+
+            while (j > 0 && intArray[j - 1] > intArray[j]){
+                swap(intArray, j, j - 1);
+                j--;
+            }
+        }
+        return intArray;
+
     }
 
     // Selection Sort
@@ -74,54 +104,27 @@ public class Main {
         return intArray;
     }
 
-    // Insertion Sort
-    private static int[] insertionSort(int[] intArray){
+    // Bubble Sort
+    private static int[] bubbleSort(int[] intArray){
+        // unsortedPartitionIndex (last index of the unsorted array)
+        // i = 0, index used to traverse the array from left to right
 
-        // In-place algorithm
+        // In-place algorithm (do not need to create another array to implement the algorithm. Saves memory
         // O(n2) time complexity - quadratic
+        // Algorithm degrades quickly
         // Stable algorithm
 
-        for (int firstUnsortedIndex = 1; firstUnsortedIndex < intArray.length; firstUnsortedIndex++) {
+        for (int lastUnsortedIndex = intArray.length - 1; lastUnsortedIndex > 0; lastUnsortedIndex--){
 
-            int checkElement = intArray[firstUnsortedIndex];
-
-            for (int sortedIndex = 0; sortedIndex <= firstUnsortedIndex ; sortedIndex++) {
-
-                if (checkElement < intArray[sortedIndex]){
-                    swap(intArray, checkElement, intArray[sortedIndex]);
-                    break;
+            for (int i = 0; i < lastUnsortedIndex; i++){
+                if (intArray[i] > intArray[i + 1]){
+                    swap(intArray, i, i +1);
                 }
             }
         }
+
         return intArray;
     }
-
-    // Shell Sort
-    private static int[] shellSort(int[] intArray){
-        // Variation of Insertion sort
-        // In-place algorithm
-        // O(n2) worst case, but it can perform much better than that (depends on the gap)
-        // Unstable algorithm
-
-        for (int gap = intArray.length / 2; gap > 0; gap /= 2) {
-
-            for (int i = gap; i < intArray.length ; i++) {
-                int checkElement = intArray[i];
-
-                int j = i;
-                System.out.println(String.format("Value of j = %d and gap = %d", j, gap));
-
-                while (j >= gap && intArray[j - gap] > checkElement){
-                    intArray[j] = intArray[j - gap];
-                    j -= gap;
-                }
-                intArray[j] = checkElement;
-            }
-        }
-        return intArray;
-    }
-
-
 
 } // end of Main
 
